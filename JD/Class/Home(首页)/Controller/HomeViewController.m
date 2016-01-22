87f584ebde1b2d9640394d8cb1dea09304f504c2
lizard77 <18768979517@163.com>
2016-01-22 17:47:45 +0800

    1.22  pm

--- JD/Class/Home(首页)/Controller/HomeViewController.m
+++ JD/Class/Home(首页)/Controller/HomeViewController.m
@@ -7,10 +7,14 @@
 //
 
 #import "HomeViewController.h"
+#import "SearchBarView.h"
+#import "SDCycleScrollView.h"
+#import "SDRefresh.h"
+#import "MJRefresh.h"
 //#import "Header.h"
-@interface HomeViewController ()<UITableViewDelegate,UITableViewDataSource>
+@interface HomeViewController ()<UITableViewDelegate,UITableViewDataSource,SearchBarViewDelegate,SDCycleScrollViewDelegate>
 @property (nonatomic,strong)UITableView *tableView;
-
+@property (nonatomic,strong)SDCycleScrollView *scroll;
 
 @end
 
@@ -19,6 +23,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self loadTableView];
+    [self setNavgationItem];
+    [self setupRefresh];
     // Do any additional setup after loading the view.
 }
 - (void)loadTableView{
@@ -27,13 +33,56 @@
     _tableView.delegate = self;
     _tableView.dataSource = self;
     [self.view addSubview:_tableView];
+    _tableView.tableHeaderView = self.scroll;
     
     
 }
+- (void)setNavgationItem{
+    self.navigationController.navigationBar.backgroundColor = [UIColor redColor];
+    self.navigationItem.leftBarButtonItem = [UIBarButtonItem BarButtonItemWithImageName:@"ico_camera_7" highImageName:nil title:@"扫啊扫" target:self action:@selector(camerca)];
+    self.navigationItem.rightBarButtonItem = [UIBarButtonItem BarButtonItemWithImageName:@"HomePage_Message" highImageName:nil title:@"消息中心" target:self  action:@selector(message)];
+    SearchBarView *search = [[SearchBarView alloc] initWithFrame:CGRectMake(0, 7, 240, 30)];
+    search.delegate = self;
+    self.navigationItem.titleView = search;
+    
+}
+- (UIView *)scroll{
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSArray *image = @[
+                                  @"http://img30.360buyimg.com/mobilecms/s480x180_jfs/t1402/221/421883372/88115/8cc2231a/55815835N35a44559.jpg",
+                                  @"http://img30.360buyimg.com/mobilecms/s480x180_jfs/t976/208/1221678737/91179/5d7143d5/5588e849Na2c20c1a.jpg",
+                                  @"http://img30.360buyimg.com/mobilecms/s480x180_jfs/t805/241/1199341035/289354/8648fe55/5581211eN7a2ebb8a.jpg",
+                                  @"http://img30.360buyimg.com/mobilecms/s480x180_jfs/t1606/199/444346922/48930/355f9ef/55841cd0N92d9fa7c.jpg",
+                                  @"http://img30.360buyimg.com/mobilecms/s480x180_jfs/t1609/58/409100493/49144/7055bec5/557e76bfNc065aeaf.jpg",
+                                  @"http://img30.360buyimg.com/mobilecms/s480x180_jfs/t895/234/1192509025/111466/512174ab/557fed56N3e023b70.jpg",
+                                  @"http://img30.360buyimg.com/mobilecms/s480x180_jfs/t835/313/1196724882/359493/b53c7b70/5581392cNa08ff0a9.jpg",
+                                  @"http://img30.360buyimg.com/mobilecms/s480x180_jfs/t898/15/1262262696/95281/57d1f12f/558baeb4Nbfd44d3a.jpg"
+                                  ];
+    _scroll = [SDCycleScrollView cycleScrollViewWithFrame:CGRectMake(0, 0, mainWidth, 180) imageURLStringsGroup:image];
+    _scroll.infiniteLoop = YES;
+    _scroll.delegate = self;
+    _scroll.placeholderImage = [UIImage imageNamed:@"homepagebannerplaceholder"];
+    _scroll.pageControlStyle = SDCycleScrollViewPageContolStyleClassic;
+    _scroll.autoScrollTimeInterval = 2.0;
+    //_scroll.imageURLStringsGroup = image;
+
+    return _scroll;
+    
+}
+#pragma mark  加载下拉刷新
+- (void)setupRefresh{
+    [self.tableView addLegendHeaderWithRefreshingTarget:self refreshingAction:@selector(loadNewDate)];
+    //LegendHeaderWithRefreshingBlock:^(){
+//        [self.tableView reloadData];
+//        [self.tableView.header endRefreshing];}];
+                         //GifHeaderWithRefreshingTarget:self refreshingAction:@selector(loadNewDate)];
+    
+    
+
+}
+- (void)loadNewDate{
+    [self.tableView reloadData];
+    [self.tableView.header endRefreshing];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -57,6 +106,13 @@ static NSString *identy = @"cell";
 }
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
     return 0.1;
+}
+#pragma mark 导航栏
+- (void)camerca{
+    DLog(@"拍照吗？");
+}
+- (void)message{
+    DLog(@"亲，您有新短消息");
 }
 /*
 #pragma mark - Navigation
